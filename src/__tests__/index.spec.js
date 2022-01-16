6e4e04c78a95eee4723ea1a37e543de43ec17c19
Bianca Salino <biancadsalino@gmail.com>
2022-01-16 20:16:14 -0300

    implementando testes

--- src/__tests__/index.spec.js
+++ src/__tests__/index.spec.js
@@ -1,7 +1,38 @@
 import { humanizeList } from "../index";
 
-describe("test", () => {
-  it("works", () => {
-    expect(humanizeList()).toBe(true);
+describe("humanizeList", () => {
+  it("deve ser retornado o elemento da lista quando for conjuncao", () => {
+    const resultado = humanizeList([1], 'conjunction')
+    expect(humanizeList(resultado)).toBe('1');
   });
+
+  it("deve ser retornado o elemento da lista quando for disjuncao", () => {
+    const resultado = humanizeList([1], 'disjunction')
+    expect(humanizeList(resultado)).toBe('1');
+  });
+
+  it("quando for um array de 3 itens deve ser retornado os elementos com o conectivo and quando for conjuncao", () => {
+    const resultado = humanizeList([1, 2, 3], 'conjunction')
+    expect(resultado).toBe("1, 2 and 3")
+  })
+
+  it("quando for um array de 3 itens deve ser retornado os elementos com o conectivo or quando for disjuncao", () => {
+    const resultado = humanizeList([1, 2, 3], 'disjunction')
+    expect(resultado).toBe("1, 2 or 3")
+  })
+
+  it("quando for um array de 2 itens deve ser retornado os elementos com o conectivo and quando for conjuncao", () => {
+    const resultado = humanizeList([1, 2], 'conjunction')
+    expect(resultado).toBe("1 and 2")
+  })
+
+  it("quando for um array de 2 itens deve ser retornado os elementos com o conectivo or quando for disjuncao", () => {
+    const resultado = humanizeList([1, 2], 'disjunction')
+    expect(resultado).toBe("1 or 2")
+  })
+
+  it("quando for um array de 5 elementos deve retornar os elementos com o limite e o conectivo AND quando for conjuncao ", () => {
+    const resultado = humanizeList([1, 2, 3, 4, 5], 'conjunction', 3)
+    expect(resultado).toBe("1, 2 ... and 5")
+  })
 });
--- src/index.js
+++ src/index.js
@@ -8,4 +8,21 @@ limit: number
 string
 */
 
-export const humanizeList = () => {};
+export const humanizeList = (list, type, limite) => {
+    const conectivo = type === 'conjunction' ? 'and' : 'or'
+
+    if (list.length > 3) {
+        const firstPart = list.slice(0, limite - 1)
+        const lastPart = list[list.length - 1]
+
+        return `${firstPart.join(', ')} ... ${conectivo} ${lastPart}`
+    }
+
+    if (list.length === 3)
+        return `${list[0]}, ${list[1]} ${conectivo} ${list[2]}`
+
+    if (list.length === 2)
+        return `${list[0]} ${conectivo} ${list[1]}`
+
+    return `${list[0]}`
+};
